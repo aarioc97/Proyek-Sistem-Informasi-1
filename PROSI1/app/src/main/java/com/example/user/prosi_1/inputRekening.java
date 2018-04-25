fec24cd3f888238898eac96ac75c556b059c1197
arialesmana <ariaalesmana@yahoo.com>
2018-04-25 23:41:01 +0700

    changes 25/04/2018

--- PROSI1/app/src/main/java/com/example/user/prosi_1/inputRekening.java
+++ PROSI1/app/src/main/java/com/example/user/prosi_1/inputRekening.java
@@ -2,15 +2,12 @@ package com.example.user.prosi_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
-class PilihRole extends AppCompatActivity {
+public class inputRekening extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pilih_role);
+        setContentView(R.layout.activity_input_rekening);
     }
 }
