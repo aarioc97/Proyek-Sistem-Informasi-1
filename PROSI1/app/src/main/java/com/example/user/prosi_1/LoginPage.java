e6f7e38ad8b4d92127355ec129b0ab4d6362df78
arialesmana <38497411+arialesmana@users.noreply.github.com>
2018-05-05 14:39:17 +0700

    connect to url

--- PROSI1/app/src/main/java/com/example/user/prosi_1/LoginPage.java
+++ PROSI1/app/src/main/java/com/example/user/prosi_1/LoginPage.java
@@ -1,5 +1,6 @@
 package com.example.user.prosi_1;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -8,17 +9,31 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginPage extends AppCompatActivity implements View.OnClickListener {
 
     Button login_to_app;
-    Connection con;
-    String un, pass, db, ip;
     EditText username, password;
+    RequestQueue requestQueue;
+    private static final String URL = "http://127.0.0.1/";
+    private StringRequest request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,55 +51,39 @@ public class LoginPage extends AppCompatActivity implements View.OnClickListener
 
     @Override
     public void onClick(View view) {
-        String restURL = "flynbuy.com";
-
-    }
-
-    public class checkLogin extends AsyncTask<String, String, String> {
-
-        String z = "";
-        Boolean isSuccess = false;
-
-        @Override
-        protected String doInBackground(String... strings) {
-            String usernam = username.getText().toString();
-            String passwordd = password.getText().toString();
-
-            if (usernam.trim().equals("") || passwordd.trim().equals("")) {
-                z = "Please enter username and password";
-            } else {
-                try {
-                    con = connectionclass(un, pass, db, ip);
-                    if (con == null) {
-                        z = "Check your internet access";
-                    } else {
-                        String query = "select * from login where username = '" + usernam.toString() + "' and password = '" + passwordd.toString() + "'";
+        if(view == login_to_app){
+            request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try{
+                        JSONObject jsonObject = new JSONObject(response);
+                        if(jsonObject.names().get(0).equals("success")){
+                            Toast.makeText(getApplicationContext(),"SUCCESS"+jsonObject.getString("success"),Toast.LENGTH_SHORT);
+                            startActivity(new Intent(getApplicationContext(),Home.class));
+                        }else{
+                            Toast.makeText(getApplicationContext(),"Error"+jsonObject.getString("error"),Toast.LENGTH_SHORT);
+                        }
+                    }catch (JSONException e){
+                        e.printStackTrace();
                     }
-                } catch (Exception ex) {
-                    isSuccess = false;
-                    z = ex.getMessage();
                 }
-            }
-            return z;
-        }
-    }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
 
-    public Connection connectionclass(String user, String password, String database, String server) {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        Connection connection = null;
-        String ConnectionURL = null;
-        try {
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            ConnectionURL = "flynbuy.com";
-            connection = DriverManager.getConnection(ConnectionURL);
-        } catch (ClassNotFoundException e) {
-            Log.e("error here 1 : ", e.getMessage());
-        } catch (SQLException se) {
-            Log.e("error here 2 : ", se.getMessage());
-        } catch (Exception e) {
-            Log.e("error here 3 : ", e.getMessage());
+                }
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError{
+                    HashMap<String,String> hashMap = new HashMap<String, String>();
+                    hashMap.put("username",username.getText().toString());
+                    hashMap.put("password",password.getText().toString());
+
+                    return hashMap;
+                }
+            };
+
+            requestQueue.add(request);
         }
-        return connection;
     }
 }
