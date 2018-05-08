8d973ab275bda7066849d9af982d5aa93af8c5bc
arialesmana <38497411+arialesmana@users.noreply.github.com>
2018-05-08 14:57:33 +0700

    connection

--- PROSI1/app/src/main/java/com/example/user/prosi_1/LoginPage.java
+++ PROSI1/app/src/main/java/com/example/user/prosi_1/LoginPage.java
@@ -1,15 +1,6 @@
-package com.example.user.prosi_1;
-
-<<<<<<< HEAD
-import android.app.ProgressDialog;
-=======
 import android.content.Intent;
->>>>>>> 8cac79a609f9f806e1dda5cbd5098911091c00c2
-import android.os.AsyncTask;
-import android.os.StrictMode;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,160 +12,76 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.user.prosi_1.Home;
+import com.example.user.prosi_1.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoginPage extends AppCompatActivity implements View.OnClickListener {
+public class LoginPage extends AppCompatActivity {
 
-<<<<<<< HEAD
-    Button loginToApp;
-    Connection con;
-    String un, pass, db, ip;
-=======
-    Button login_to_app;
-    EditText username, password;
-    RequestQueue requestQueue;
-    private static final String URL = "http://127.0.0.1:80/flynbuy/login_control.php";
+    private EditText email,password;
+    private Button sign_in_register;
+    private RequestQueue requestQueue;
+    private static final String URL = "http://192.168.1.65:80/flynbuy/login_control.php";
     private StringRequest request;
->>>>>>> 8cac79a609f9f806e1dda5cbd5098911091c00c2
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
-        loginToApp = this.findViewById(R.id.btn_login_to_app);
-        loginToApp.setOnClickListener(this);
-    }
+        email = (EditText) findViewById(R.id.et_username_login);
+        password = (EditText) findViewById(R.id.et_password_login);
+        sign_in_register = (Button) findViewById(R.id.btn_login_to_app);
 
-    @Override
-    public void onClick(View view) {
-<<<<<<< HEAD
-        String restURL = "http://www.flynbuy.com";
-        new CheckLogin().execute(restURL);
-    }
+        requestQueue = Volley.newRequestQueue(this);
 
-    private class CheckLogin extends AsyncTask<String, String, String> {
+        sign_in_register.setOnClickListener(new View.OnClickListener() {
 
-        final HttpClient httpClient = new DefaultHttpClient();
-        String content, error, data = "";
-        ProgressDialog progressDialog = new ProgressDialog(LoginPage.this);
-        EditText username = findViewById(R.id.et_username_login);
-        EditText password = findViewById(R.id.et_password_login);
+            @Override
+            public void onClick(View view) {
 
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-            progressDialog.setTitle("Please wait...");
-            progressDialog.show();
-            try {
-                data += "&" + URLEncoder.encode("data", "UTF-8") + "-" + username.getText();
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-        }
+                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject.names().get(0).equals("success")){
+                                Toast.makeText(getApplicationContext(),"SUCCESS "+jsonObject.getString("success"),Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),Home.class));
+                            }else {
+                                Toast.makeText(getApplicationContext(), "Error" +jsonObject.getString("error"), Toast.LENGTH_SHORT).show();
+                            }
 
-        @Override
-        protected String doInBackground(String... strings) {
-
-            BufferedReader br = null;
-            try{
-                URL url = new URL(params[0]);
-                URLConnection conn = url.openConnection();
-                conn.setDoOutput(true);
-            } catch (MalformedURLException e){
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-
-            if (usernam.trim().equals("") || passwordd.trim().equals("")) {
-                z = "Please enter username and password";
-            } else {
-                try {
-                    con = connectionclass(un, pass, db, ip);
-                    if (con == null) {
-                        z = "Check your internet access";
-                    } else {
-                        String query = "select * from login where username = '" + usernam.toString() + "' and password = '" + passwordd.toString() + "'";
-=======
-        if(view == login_to_app){
-            request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    try{
-                        JSONObject jsonObject = new JSONObject(response);
-                        if(jsonObject.names().get(0).equals("success")){
-                            Toast.makeText(getApplicationContext(),"SUCCESS"+jsonObject.getString("success"),Toast.LENGTH_SHORT);
-                            startActivity(new Intent(getApplicationContext(),Home.class));
-                        }else{
-                            Toast.makeText(getApplicationContext(),"Error"+jsonObject.getString("error"),Toast.LENGTH_SHORT);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                    }catch (JSONException e){
-                        e.printStackTrace();
->>>>>>> 8cac79a609f9f806e1dda5cbd5098911091c00c2
+
+
                     }
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
 
-                }
-            }){
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError{
-                    HashMap<String,String> hashMap = new HashMap<String, String>();
-                    hashMap.put("username",username.getText().toString());
-                    hashMap.put("password",password.getText().toString());
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        HashMap<String,String> hashMap = new HashMap<String, String>();
+                        hashMap.put("email",email.getText().toString());
+                        hashMap.put("password",password.getText().toString());
 
-                    return hashMap;
-                }
-            };
+                        return hashMap;
+                    }
+                };
 
-<<<<<<< HEAD
-    @Override
-    protected void onPostExecute(Void res){
-
-    }
-
-    //--------------------------------------------//
-
-    public Connection connectionclass(String user, String password, String database, String server) {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        Connection connection = null;
-        String ConnectionURL = null;
-        try {
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            ConnectionURL = "flynbuy.com";
-            connection = DriverManager.getConnection(ConnectionURL);
-        } catch (ClassNotFoundException e) {
-            Log.e("error here 1 : ", e.getMessage());
-        } catch (SQLException se) {
-            Log.e("error here 2 : ", se.getMessage());
-        } catch (Exception e) {
-            Log.e("error here 3 : ", e.getMessage());
-=======
-            requestQueue.add(request);
->>>>>>> 8cac79a609f9f806e1dda5cbd5098911091c00c2
-        }
+                requestQueue.add(request);
+            }
+        });
     }
 }
