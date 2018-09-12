18b9982cd4d97294e0ac5259efa34db68a883ef6
CaptObvious <aarioc97@gmail.com>
2018-09-12 19:08:48 +0700

    Update tampilan dan Intent.
    beberapa tampilan di(finish) saat memunculkan tampilan selanjutnya, isi tiap halaman home ada search bar.

--- PROSI1/app/src/main/java/com/example/user/prosi_1/SignUp.java
+++ PROSI1/app/src/main/java/com/example/user/prosi_1/SignUp.java
@@ -1,6 +1,8 @@
 package com.example.user.prosi_1;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,7 +26,31 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
     public void onClick(View view) {
         if(view.getId() == daftar.getId()){
             Intent intent1 = new Intent(this, PilihRole.class);
+            this.finish();
             startActivity(intent1);
         }
+    }
+
+    @Override
+    public void onBackPressed(){
+        final Intent intent = new Intent(this, SignUpLogin.class);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Apakah Anda akan membatalkan pendaftaran?");
+        builder.setCancelable(true);
+        builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+                startActivity(intent);
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
