045217fd1678ffd8b32b1d1dddc99fbc3373ff4a
CaptObvious <aarioc97@gmail.com>
2018-09-26 19:55:36 +0700

    Edit halaman Post Barang.
    menambah beberapa fitur, tinggal akses dari tab requester belum bisa.

--- PROSI1/app/src/main/java/com/example/user/prosi_1/PostBarang.java
+++ PROSI1/app/src/main/java/com/example/user/prosi_1/PostBarang.java
@@ -2,6 +2,7 @@ package com.example.user.prosi_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class PostBarang extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class PostBarang extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_barang);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
