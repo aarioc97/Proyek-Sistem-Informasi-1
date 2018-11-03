158eb04eb928c4211af16abe5e139abcebc16588
arialesmana <38497411+arialesmana@users.noreply.github.com>
2018-11-03 14:54:35 +0700

    PostBarang
    post barang udah bisa masuk database cuman gatau cara masukin gambar ke firebase

--- PROSI1/app/src/main/java/basepackage/BarangPostRequest.java
+++ PROSI1/app/src/main/java/basepackage/BarangPostRequest.java
@@ -15,9 +15,29 @@ public class BarangPostRequest extends Requester {
     String statusPost;
     String jenisBarang;
     String namaBarang;
-    char statusPecahBelah;
+    String kodeBarang;
+    String deskripsi;
+    String statusPecahBelah;
     int berat;
     int harga;
-    String ukuran;
+    int panjang;
+    int lebar;
     Image foto;
+
+    public BarangPostRequest(int berat,String deskripsi,Image foto, int harga, int lebar,
+                             String namaBarang, int panjang, String statusPecahBelah,int jumlahPR){
+
+        this.berat = berat;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+        this.harga = harga;
+        this.kodeBarang = "Request";
+        this.lebar = lebar;
+        this.namaBarang = namaBarang;
+        this.panjang = panjang;
+        this.statusPecahBelah = statusPecahBelah;
+        this.jumlahPR = jumlahPR;
+
+    }
+
 }
--- PROSI1/app/src/main/java/com/example/user/prosi_1/PostBarang.java
+++ PROSI1/app/src/main/java/com/example/user/prosi_1/PostBarang.java
@@ -3,6 +3,8 @@ package com.example.user.prosi_1;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.Image;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
@@ -11,30 +13,42 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class PostBarang extends AppCompatActivity implements View.OnClickListener {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.internal.FirebaseAppHelper;
+
+import basepackage.BarangPostRequest;
+
+public class PostBarang extends AppCompatActivity implements View.OnClickListener{
 
     private static final int RESULT_LOAD_IMAGE = 1;
 
-    EditText namaBarang, kuantitas, deskripsi, kotaAsal, kotaTujuan, harga;
+    EditText nama, kuantitas, deskripsi, kotaAsal, kotaTujuan, harga, berat, panjang, lebar;
     TextView adminFee, ongkirFee, totalFee;
     Button calculate, post;
     ImageView iv_barang;
+    CheckBox cb_status;
+    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_barang);
 
-        namaBarang = this.findViewById(R.id.et_nama_barang);
+        nama = this.findViewById(R.id.et_nama_barang);
         kuantitas = this.findViewById(R.id.et_kuantitas);
         deskripsi = this.findViewById(R.id.et_deskripsi);
         kotaAsal = this.findViewById(R.id.kota_asal);
         kotaTujuan = this.findViewById(R.id.kota_tujuan);
         harga = this.findViewById(R.id.et_harga);
+        berat = this.findViewById(R.id.et_berat);
+        panjang = this.findViewById(R.id.et_panjang);
+        lebar = this.findViewById(R.id.et_lebar);
 
         adminFee = this.findViewById(R.id.tv_admin_fee);
         ongkirFee = this.findViewById(R.id.tv_ongkir_fee);
@@ -45,6 +59,8 @@ public class PostBarang extends AppCompatActivity implements View.OnClickListene
 
         iv_barang = this.findViewById(R.id.iv_canvas_req);
 
+        cb_status = this.findViewById(R.id.checkBox2);
+
         calculate.setOnClickListener(this);
         iv_barang.setOnClickListener(this);
 
@@ -53,13 +69,44 @@ public class PostBarang extends AppCompatActivity implements View.OnClickListene
     @Override
     public void onClick(View v) {
         if (v == calculate){
-            adminFee.setText(harga.getText());
-            ongkirFee.setText("10000");
-            totalFee.setText("");
+            int hargaCalculated = Integer.parseInt(harga.getText().toString());
+            int adminFeeCalculated = hargaCalculated*25/100;
+            int ongkirFeeCalculated = hargaCalculated/10;
+
+            int totalFeeCalculated = hargaCalculated + adminFeeCalculated + ongkirFeeCalculated;
+
+            adminFee.setText(adminFeeCalculated);
+            ongkirFee.setText(ongkirFeeCalculated);
+            totalFee.setText(totalFeeCalculated);
         }
         if (v == iv_barang){
             Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             startActivityForResult(galleryIntent,RESULT_LOAD_IMAGE);
+        }
+        if(v == post){
+            int beratBarang = Integer.parseInt(berat.getText().toString());
+            String deskripsiBarang = deskripsi.getText().toString();
+            Image gambarBarang = null;
+            int hargaBarang = Integer.parseInt(harga.getText().toString());
+            int lebarBarang = Integer.parseInt(lebar.getText().toString());
+            String namaBarang = nama.getText().toString();
+            int panjangBarang = Integer.parseInt(panjang.getText().toString());
+            String statusBarang="";
+
+            if(cb_status.isChecked()){
+                statusBarang = "Ya";
+            }
+            else if(!cb_status.isChecked()){
+                statusBarang = "Tidak";
+            }
+
+            int kuantitasBarang = Integer.parseInt(kuantitas.getText().toString());
+
+
+            BarangPostRequest barang = new BarangPostRequest(beratBarang,deskripsiBarang,gambarBarang,hargaBarang,lebarBarang,namaBarang,
+                    panjangBarang,statusBarang,kuantitasBarang);
+
+            mDatabase.child("barang").setValue(barang);
         }
     }
 
